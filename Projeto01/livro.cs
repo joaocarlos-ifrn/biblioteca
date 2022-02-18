bc78448cdeb098dacbe929a1a2e7b1688f4287b9
JOAOCARLOS55 <>
2022-02-18 13:09:44 +0000

    Classe Livro

--- Projeto01/livro.cs
+++ Projeto01/livro.cs
@@ -1,5 +1,31 @@
 using System;
 
 class Livro {
+  private string nome;
+  private string isbn;
   
+  public Livro(string nome, string isbn){
+    this.nome = nome;
+    this.isbn = isbn;
+  }
+
+  public void SetNome(string nome){
+    this.nome = nome;
+  }
+
+  public void SetIsbn(string isbn){
+    this.isbn = isbn;
+  }
+
+  public string GetNome(){
+    return nome;
+  }
+
+  public string GetIsbn(){
+    return isbn;
+  }
+
+  public override string ToString(){
+    return $"{nome} - {isbn}";
+  }
 }
--- Projeto01/main.cs
+++ Projeto01/main.cs
@@ -5,11 +5,18 @@ class Program {
 
   Console.WriteLine("Bem-vindo à Biblioteca");
 
-  //objetos de Especie
+  //objetos de Genero
     Genero g1 = new Genero("Aventura");
     Genero g2 = new Genero("Romance");
 
+  //objetos de Livro
+    Livro l1 = new Livro("Apple", "1");
+    Livro l2 = new Livro("Meta", "2");
+
     Console.WriteLine(g1);
     Console.WriteLine(g2);
+
+    Console.WriteLine(l1);
+    Console.WriteLine(l2);
   }
 }
